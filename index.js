96aa24b80873dfb5b9e3bf93114549a5044a5c30
bootcamp <myezasifiso11@gmail.com>
2017-07-25 11:21:02 +0200

    renders a list of registration numbers

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.engine( 'hbs', hbs( { extname: 'hbs', defaultLayout: 'main'}))
 app.set( 'view engine', 'hbs' );
 
 app.use(express.static('public'));
 
 // create a route
-app.get('/reg_number/:registration', function (req, res) {
-  var reg_num = req.params.registration
- res.render('registration_number',{reg_num});
+app.get('/reg_numbers', function (req, res) {
+  // var reg_num = req.body.reg_number;
+ res.render('registration_number');
+});
+var reg_numbers =[];
+app.post('/reg_numbers', function (req, res) {
+    var reg_num = req.body.reg_number;
+    reg_numbers.push(reg_num)
+ res.render('registration_number',{reg_numbers:reg_numbers});
 });
 
 //start the server
